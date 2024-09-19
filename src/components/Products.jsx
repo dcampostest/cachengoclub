f4deafa19e408285f406bb1ee11064bd9deede74
Darian <darian@MacBook-Pro-de-Darian.local>
2024-09-19 03:06:59 -0300

    update

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,17 +4,20 @@ import generic from '../images/Cachengo_SinFondo_7.png'
 import autoria from '../images/autoria.png'
 import papas from '../images/papas.png'
 import pizzas from '../images/pizza.jpeg'
-import burguer from '../images/burguer.jpeg'
+import burguer from '../images/burguer.jpg'
 import cocktails from '../images/cocktails.png'
 import birras from '../images/birras.png'
 import vinos from '../images/vinos.png'
 import mocktails from '../images/mocktails.png'
 import combos from '../images/combos.png'
+import pintas from '../images/pintas.jpg'
+import individuales from '../images/individuales.png'
+import medidas from '../images/medidas.png'
 
 function Products(){
     return(
         <div id='products'>
-            <h3>BIENVENIDA PRIMAVERA</h3>
+            <h3 class="gradient-text">BIENVENIDA PRIMAVERA</h3>
             <p></p>
             <div className='a-container'>
                 <Productbox image={papas} title="Para Picotear"/>
@@ -22,13 +25,13 @@ function Products(){
                 <Productbox image={burguer} title="Burguers"/>
                 <Productbox image={autoria} title="Autoría"/>
                 <Productbox image={cocktails} title="Cocktails"/>
-                <Productbox image={birras} title="Pintas"/>
-                <Productbox image={generic} title="Individuales"/>
+                <Productbox image={pintas} title="Pintas"/>
+                <Productbox image={individuales} title="Individuales"/>
                 <Productbox image={mocktails} title="Mocktails"/>
                 <Productbox image={generic} title="Gintoneria"/>
                 <Productbox image={birras} title="Birritas Industriales"/>
                 <Productbox image={combos} title="Combos"/>
-                <Productbox image={generic} title="Medidas"/>
+                <Productbox image={medidas} title="Medidas"/>
                 <Productbox image={vinos} title="Vinos"/>
             </div>
         </div>
