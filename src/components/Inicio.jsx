e8c8e4224d7e690ddc8a4baf8141889b6570c5f1
Darian <darian@MacBook-Pro-de-Darian.local>
2024-09-19 02:53:19 -0300

    update

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -7,7 +7,6 @@ function Inicio(){
                 <h4>Bienvenidos #THECLUB</h4>
                 <h2><span>CACHENGO</span> FOR<br/>EVER</h2>
                 <p className='details'>Viernes & Sábados a partir de las 21 hs.</p>
-                <h3 style={{color: '#FFC107'}}>BIENVENIDA PRIMAVERA</h3>
             </div>
         </div>
     )
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,25 +1,35 @@
 import React from "react";
 import Productbox from "./Productbox";
 import generic from '../images/Cachengo_SinFondo_7.png'
+import autoria from '../images/autoria.png'
+import papas from '../images/papas.png'
+import pizzas from '../images/pizza.jpeg'
+import burguer from '../images/burguer.jpeg'
+import cocktails from '../images/cocktails.jpeg'
+import birras from '../images/birras.png'
+import vinos from '../images/vinos.png'
+import mocktails from '../images/mocktails.png'
+import combos from '../images/combos.png'
+
 function Products(){
     return(
         <div id='products'>
-            <h1>Carta</h1>
+            <h3>BIENVENIDA PRIMAVERA</h3>
             <p></p>
             <div className='a-container'>
-                <Productbox image={generic} title="Para Picotear"/>
-                <Productbox image={generic} title="Pizzas"/>
-                <Productbox image={generic} title="Burguers"/>
-                <Productbox image={generic} title="Autoría"/>
-                <Productbox image={generic} title="Cocktails"/>
-                <Productbox image={generic} title="Pintas"/>
+                <Productbox image={papas} title="Para Picotear"/>
+                <Productbox image={pizzas} title="Pizzas"/>
+                <Productbox image={burguer} title="Burguers"/>
+                <Productbox image={autoria} title="Autoría"/>
+                <Productbox image={cocktails} title="Cocktails"/>
+                <Productbox image={birras} title="Pintas"/>
                 <Productbox image={generic} title="Individuales"/>
-                <Productbox image={generic} title="Mocktails"/>
+                <Productbox image={mocktails} title="Mocktails"/>
                 <Productbox image={generic} title="Gintoneria"/>
-                <Productbox image={generic} title="Birritas Industriales"/>
-                <Productbox image={generic} title="Combos"/>
+                <Productbox image={birras} title="Birritas Industriales"/>
+                <Productbox image={combos} title="Combos"/>
                 <Productbox image={generic} title="Medidas"/>
-                <Productbox image={generic} title="Vinos"/>
+                <Productbox image={vinos} title="Vinos"/>
             </div>
         </div>
     )
